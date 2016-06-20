cd7aadb69d040d948013e60bd59ce040c23a5694
Allen Guan <apguan@gmail.com>
2016-06-19 20:59:09 -0700

    filters and removes duplicats

---
+++ Filter Function.js
@@ -0,0 +1,20 @@
+function uniteUnique(arr) {
+ //need to concat into one single array
+
+ var filterCallBack = function(value, index) {
+    return concatArr.indexOf(value) == index; 
+ };
+ 
+ var concatArr = [];
+ for (var i=0; i<arguments.length; i++){
+  concatArr = concatArr.concat(arguments[i]); 
+ }
+  
+ uniqueArray = concatArr.filter(filterCallBack);
+ 
+ return uniqueArray;
+    
+}
+
+//sample data
+arr = [1,2,4,6,34,745, ["Hello World"], "Hello World", 1,375,34]
